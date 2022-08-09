cf93992ad6ecb711e3b8a0f0cc02e38996d94f6a
Taeho Park <qkrxogh7@gmail.com>
2022-08-09 13:31:44 +0900

    add 그래프이론

--- 백준문제/5일차/test.py
+++ 백준문제/5일차/test.py
@@ -1,20 +1,6 @@
-# 3 x 3 행렬
-from pprint import pprint
-n = 8
-m = 7
-hang = []
-# for i in range(n):
-#     hang.append([i]*m)
-# pprint(hang)
-
-# print(hang.append([list(map(int,input().split())) for _ in range(n)]))
-'''
-n x m 크기의 입력을 받아보자
-3 4
-1 2 3 4 
-5 6 7 8 
-9 0 1 2
-
-'''
-result = [(list(map(int, input().split()))) for _ in range(3)]
-pprint(result)
+total = 0
+m = 0
+while n < 10:
+    n = n + 1
+    total = total + n
+    print(f'{n}까지 합{total}')
