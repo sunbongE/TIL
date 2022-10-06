0ab7e7e8a1028e5f2cb1d4508b649f1aa4209e77
taehopark <qkrxogh7@gmail.com>
2022-10-06 22:25:58 +0900

    D3

--- SWEA/D3/1228.py
+++ SWEA/D3/1228.py
@@ -8,15 +8,14 @@ for _ in range(10):
 
     com_cnt = int(input())
     com_li = input().split()
-    com = []
     
     for i in range(len(com_li)):
         if com_li[i] == 'I':
             loc=int(com_li[i+1])
-            cnt =int(com_li[i+2])
-            for n in range(cnt,0,-1):
-                origin.insert(loc,com_li[i+2+n]) 
-                # print(loc+1,com_li[i+2+n])
+            cnt =int(com_li[i+2])                   # I위치에서 2칸 뒤는 반복 횟수 즉,y의 위치
+            for n in range(cnt,0,-1):               # 뒤에서부터 삽입
+                origin.insert(loc,com_li[i+2+n])    #
+                # print(loc,com_li[i+2+n])
     print(f'#{_+1}',end=' ')
     print(*origin[:10])
 
