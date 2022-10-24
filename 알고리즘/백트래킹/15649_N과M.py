298eba523c711ad613d7feb8912df6135eb26901
taehopark <qkrxogh7@gmail.com>
2022-10-25 00:01:48 +0900

    백트래킹

---
+++ 알고리즘/백트래킹/15649_N과M.py
@@ -0,0 +1,22 @@
+# 자연수 N과 M이 주어졌을 때,
+# 아래 조건을 만족하는 길이가 M인 수열을 모두 구하는 프로그램을 작성하시오.
+# 1부터 N까지 자연수 중에서 중복 없이 M개를 고른 수열
+# 
+
+n,m = list(map(int,input().split()))
+ 
+s = []
+ 
+def dfs():
+    if len(s)==m:
+        print(' '.join(map(str,s)))
+        return
+    
+    for i in range(1,n+1):
+        if i not in s:
+            s.append(i)
+            dfs()
+            s.pop()
+ 
+dfs()
+ 
