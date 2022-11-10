d8c4c73bf1926a694050abe5fe5944e6c1626e53
taehopark <qkrxogh7@gmail.com>
2022-11-10 21:37:45 +0900

    크롤링 연습

--- 크롤링연습/test.py
+++ 크롤링연습/test.py
@@ -7,6 +7,16 @@ from selenium.webdriver.support.ui import WebDriverWait
 import time
 import urllib.request # 이미를 받기위한 라이브러리
 
+test_result = {
+    'image':[],
+    'name': [],
+    'content':[],
+    'size':[],
+    'price':[]
+}
+
+
+
 options = webdriver.ChromeOptions()
 options.add_experimental_option("excludeSwitches", ["enable-logging"])
 
@@ -16,7 +26,6 @@ driver = webdriver.Chrome(
 
 URL = "https://www.zara.com/kr/ko/search"
 driver.get(url=URL)
-
 driver.implicitly_wait(5)
 
 # 검색창의 아이디를 선택
@@ -26,7 +35,6 @@ elem = driver.find_element(By.ID, 'search-products-form-combo-input')
 elem.send_keys("자켓")
 elem.send_keys(Keys.RETURN)
 time.sleep(3)
-# SCROLL_PAUSE_TIME = 3
 
 
 url_ = driver.find_elements(By.CLASS_NAME,'product-link')
@@ -37,51 +45,59 @@ for u in url_:
         lst.append(u.get_attribute("href"))
         # print(u.get_attribute("href"))
     cnt += 1
-# print(lst)
-for l in lst:
-    URL = l
-    driver.get(url=URL)
+print(len(lst))
+# num=1
+# for l in lst:
+#     if num == 50:
+#         break
+#     URL = l
+#     driver.get(url=URL)
+#     driver.implicitly_wait(5)
 
-    driver.implicitly_wait(5)
-
-    images = driver.find_elements(
-        By.CLASS_NAME,
-        "media-image",
-    )
-    # 이미지 링크 받아옴
-    for img in images:
-        img = img.find_element(By.TAG_NAME, "source")
-        img_list = img.get_attribute("srcset").split()
-        print(img_list[-2])
+#     images = driver.find_elements(
+#         By.CLASS_NAME,
+#         "media-image",
+#     )
+#     # 이미지 링크 받아옴
+#     for img in images:
+#         img = img.find_element(By.TAG_NAME, "source")
+#         img_list = img.get_attribute("srcset").split()
+#         print(img_list[-2])
+#         # test_result['image'].append(img_list[-2])
     
-    # 상품 이름
-    name = driver.find_element(
-    By.CLASS_NAME,
-    "product-detail-info__header-name",
-    )
-    print(name.text)
+#     # 상품 이름
+#     name = driver.find_element(
+#     By.CLASS_NAME,
+#     "product-detail-info__header-name",
+#     )
+#     print(name.text)
+#     # test_result['name'].append(name.text)
 
-    # 상품 설명
-    content = driver.find_element(
-    By.XPATH,
-    "/html/body/div[1]/div[1]/div[1]/div/div/div[2]/main/article/div/div[1]/div[2]/div[1]/div[2]/div/div/div/p",
-    )
-    print(content.text)
+#     # 상품 설명
+#     content = driver.find_element(
+#     By.XPATH,
+#     "/html/body/div[1]/div[1]/div[1]/div/div/div[2]/main/article/div/div[1]/div[2]/div[1]/div[2]/div/div/div/p",
+#     )
+#     print(content.text)
+#     # test_result['content'].append(content.text)
 
-    # 가격
-    price = driver.find_element(
-        By.CLASS_NAME,
-        "money-amount__main",
-    )
-    print(price.text)
+#     # 가격
+#     price = driver.find_element(
+#         By.CLASS_NAME,
+#         "money-amount__main",
+#     )
+#     print(price.text)
+#     # test_result['price'].append(price.text)
 
-    # 사이즈 정보
-    sizes = driver.find_elements(
-        By.CLASS_NAME,
-        "product-size-info__main-label",
-    )
-    for size in sizes:
-        print(f"size: {size.text}")
-
+#     # 사이즈 정보
+#     sizes = driver.find_elements(
+#         By.CLASS_NAME,
+#         "product-size-info__main-label",
+#     )
+#     for size in sizes:
+#         print(f"size: {size.text}")
+#         # test_result['size'].append(size.text)
+#     num += 1
 
     
+# # print(test_result)
