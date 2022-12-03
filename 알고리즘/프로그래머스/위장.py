3c4e1f5ff4d74a40001ecdd89d6a7ee47df6951f
taehopark <qkrxogh7@gmail.com>
2022-12-03 17:12:16 +0900

    폴더 정리

---
+++ 알고리즘/프로그래머스/위장.py
@@ -0,0 +1,13 @@
+def solution(clothes):
+    answer = 1
+    dtn = dict()
+
+    for i,k in clothes:
+        dtn[k] = dtn.get(k,0)+1
+    for k in dtn:
+        # print(dtn[k])
+        answer *= (dtn[k]+1)
+        # print(answer)
+    return answer-1
+
+solution([["yellow_hat", "headgear"], ["blue_sunglasses", "eyewear"], ["green_turban", "headgear"]])    	
