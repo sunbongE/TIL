858a117d2641338f8fd5b0339abdbcaa7296e3d9
taehopark <qkrxogh7@gmail.com>
2023-01-09 21:34:35 +0900

    그리디

---
+++ 알고리즘/그리디/ATM.py
@@ -0,0 +1,20 @@
+N = int(input())    
+li=list(map(int,input().split()))
+
+li.sort()
+# ans = li[0]
+# for i in range(1,len(li)):
+#     ans += sum(li[:i+1])
+# print(ans)
+sum_time=0
+for i in range(N):
+    sum_time += li[i] * (N-i)
+    print(li[i], (N-i))
+print(sum_time)
+
+# 1 5   5명이 1분을 기다리고
+# 2 4   4명이 2분을 기다리고
+# 3 3   3명이 3분을 기다리고
+# 3 2   2명이 3분을 기다리고
+# 4 1   1명이 4분을 기다리는 루트가 가장 짧다.
+# 32
