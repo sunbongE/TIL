98ee4c4ab802c2a410e93d20c3d093339659e194
taehopark <qkrxogh7@gmail.com>
2023-01-22 00:21:49 +0900

    알고리즘

---
+++ 알고리즘/그리디/보석도둑.py
@@ -0,0 +1,26 @@
+import sys
+import heapq
+
+input = sys.stdin.readline
+N, K = map(int, input().split())
+
+jew = []
+for _ in range(N):
+    heapq.heappush(jew, list(map(int, input().split())))
+
+bags = []
+for _ in range(K):
+    bags.append(int(input()))
+bags.sort()
+
+answer = 0
+tmp_jew = []
+
+for bag in bags:
+    while jew and bag >= jew[0][0]:
+        heapq.heappush(tmp_jew, -heapq.heappop(jew)[1])
+    if tmp_jew:
+        answer -= heapq.heappop(tmp_jew)
+    elif not jew:
+        break
+print(answer)
