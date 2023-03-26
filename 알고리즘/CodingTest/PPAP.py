3aa89c1cc38c19749e32ca0f4b81b6f59fd96f0c
taehopark <qkrxogh7@gmail.com>
2023-03-26 16:26:19 +0900

    algo

--- 알고리즘/CodingTest/PPAP.py
+++ 알고리즘/CodingTest/PPAP.py
@@ -1,6 +1,6 @@
 # 반복할 작업
 # 순회하면서 ppap라는 것이 나오면 p으로 변환한다.
-# 반복문이 끝난 후 값이 ppap라면 그래도 출력하고
+# 반복문이 끝난 후 값이 ppap라면 출력하고
 # 아니면 NP출력한다.
 #
 
@@ -21,3 +21,15 @@ else:
         print("PPAP")
     else:
         print("NP")
+
+# 동작
+# [P]
+# [P, P, P]
+# [P, P, P, A]
+# [P, P, P, A, P] IF문 TRUE
+# IF ...
+# [P, P, P, A]
+# [P, P, P]
+# [P, P,]
+# [P, P, A, P] == PPAP
+# OUTPUT => PPAP
--- 알고리즘/CodingTest/문자열폭발.py
+++ 알고리즘/CodingTest/문자열폭발.py
@@ -1,3 +1,9 @@
+# 반복으로 한 글자씩 스택으로
+# 폭발 문자열의 길이만큼 검색을 해본다.
+# 스택[-LEN(폭발):] == 폭발문자열 => POP()
+# 남은 스택 출력, 비어있으면 FRULA
+
+
 word = input()
 Boom = list(map(str, input()))
 stack = []
@@ -7,6 +13,7 @@ for w in word:
     if stack[-len(Boom) :] == Boom:
         for _ in range(len(Boom)):
             stack.pop()
+# print(stack)
 if stack:
     print("".join(stack))
 else:
