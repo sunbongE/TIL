e87935025a206991d83e5b7b6594b8f5d3ae7de3
taehopark <qkrxogh7@gmail.com>
2023-04-01 14:20:20 +0900

    test

--- 알고리즘/CodingTest/test.py
+++ 알고리즘/CodingTest/test.py
@@ -1,29 +1,84 @@
-# def solution(mod1, mod2, max_range):
-#     answer = [mod1]
-#     while 1:
-#         if int(max_range) < answer[-1]:
-#             break
-#         answer.append(mod1 * 2)
-#     print(answer)
-#     return len(answer)
+def solution(n):
+    dic = {
+        "0": [2, 2],
+        "1": [0, 1],
+        "2": [0, 2],
+        "3": [0, 2],
+        "4": [0, 2],
+        "5": [0, 2],
+        "6": [1, 2],
+        "7": [2, 2],
+    }
+    answer = []
+    if n <= 7:
+        answer = dic[str(n)]
+    else:
+        cnt = n // 7
+        target = str(n % 7)
+
+        tem = dic[target]
+        if cnt <= 1:
+            answer = [tem[0] + 2, tem[1] + 2]
+        elif cnt > 1 and target == "0":
+            answer = [2 * cnt, 2 * cnt]
+        else:
+            # answer = [tem[0] + 2 * cnt, tem[1] + (2 * cnt)]
+            answer = [tem[0] + (2 * cnt), tem[1] + (2 * cnt)]
+            print("target", target)
+            print("cnt", cnt)
+    return answer
 
 
-# # solution(4, 3, 20)
-# a = [1, 2, 3]
-# b = [2, 3]
-# print(set(a) - set(b))
-def find_minimum_period(s):
-    n = len(s)
-    for i in range(1, n + 1):
-        if n % i == 0:
-            repeated = True
-            for j in range(i, n):
-                if s[j] != s[j - i]:
-                    repeated = False
-                    break
-            if repeated:
-                return i
-    return n
+print(solution(29))
+print(solution(28))
 
 
-print(find_minimum_period("abcabcabd"))
+#
+# def solution(n):
+#     weekend_counts = [[2, 2], [0, 1], [0, 2], [0, 2], [0, 2], [0, 2], [1, 2], [2, 2]]
+
+#     if n <= 7:
+#         answer = weekend_counts[n]
+#     else:
+#         cnt_weeks = n // 7
+#         weekend_index = n % 7
+
+#         weekend_counts_for_weeks = [
+#             cnt_weeks + count for count in weekend_counts[weekend_index]
+#         ]
+#         if cnt_weeks <= 1 and weekend_index != 0:
+#             answer = [count + 2 for count in weekend_counts[weekend_index]]
+#         else:
+#             answer = (
+#                 weekend_counts_for_weeks
+#                 if weekend_index != 0
+#                 else [cnt_weeks * 2, cnt_weeks * 2]
+#             )
+
+#     return answer
+
+
+# def solution(n):
+#     weekend_counts = [[2, 2], [0, 1], [0, 2], [0, 2], [0, 2], [0, 2], [1, 2], [2, 2]]
+#     answer = []
+#     if n <= 7:
+#         answer = weekend_counts[n]
+#     else:
+#         cnt = n // 7
+#         target = str(n % 7)
+
+#         tem = weekend_counts[cnt]
+#         if cnt <= 1:
+#             answer = [tem[0] + 2, tem[1] + 2]
+#         elif cnt > 1 and target == "0":
+#             answer = [2 * cnt, 2 * cnt]
+#         else:
+#             # print("?")
+#             answer = [tem[0] + 2 * cnt, tem[1] + 2 * cnt]
+#             print("target", target)
+#             print("cnt", cnt)
+#     return answer
+
+
+# print(solution(29))
+# print(solution(28))
