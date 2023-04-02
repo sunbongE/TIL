64e13e01938df1caae67e969c07652f8fbdbb59f
taehopark <qkrxogh7@gmail.com>
2023-04-02 22:51:13 +0900

    solved ac Class 3

---
+++ 알고리즘/solved_ac_3/팩토리얼0의개수.py
@@ -0,0 +1,45 @@
+# from collections import Counter
+
+
+# def fac(num):
+#     if num == 1:
+#         return 1
+
+#     return num * fac(num - 1)
+
+
+# # print(fac(int(input())))
+
+
+# result = Counter(str(fac(int(input()))))
+# # print(result)
+# print(result["0"])
+def count_trailing_zeros(n):
+    count_2 = 0
+    count_5 = 0
+
+    # 1부터 n까지의 숫자에서 2와 5의 개수를 세기
+    for i in range(1, n + 1):
+        while i % 2 == 0:
+            count_2 += 1
+            i //= 2
+        while i % 5 == 0:
+            count_5 += 1
+            i //= 5
+
+    # 2와 5의 개수 중 작은 값을 반환
+    return min(count_2, count_5)
+
+
+for i in range(10):
+    print(count_trailing_zeros(10 + i))
+# 3628800
+# 39916800
+# 479001600
+# 6227020800
+# 87178291200
+# 1307674368000
+# 20922789888000
+# 355687428096000
+# 6402373705728000
+# 121645100408832000
