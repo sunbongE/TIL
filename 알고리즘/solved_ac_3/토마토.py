70c4789e6af44a19c13e4fe09514ca04872fd3fa
taehopark <qkrxogh7@gmail.com>
2023-04-06 14:18:46 +0900

    bfs

---
+++ 알고리즘/solved_ac_3/토마토.py
@@ -0,0 +1,57 @@
+# 풀이 과정
+# 9 8 7 6 5 4
+# 8 7 6 5 4 3
+# 7 6 5 4 3 2
+# 6 5 4 3 2 1
+# 최대 값에서 -1 해줘
+# 모든 경우에 토마토가 1개 이상은 있다.
+
+# 4방향으로 탐색 후 수정된 배열에 0이 남아있우면 -1 출력한다.
+from collections import deque
+
+
+def bfs():
+    while q:
+        x, y = q.popleft()
+
+        for dx, dy in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+            nx = x + dx
+            ny = y + dy
+            if 0 <= nx < n and 0 <= ny < m and box[nx][ny] == 0:
+                box[nx][ny] = box[x][y] + 1
+                q.append((nx, ny))
+
+
+m, n = map(int, input().split())
+# 토마토가있는 박스
+box = [list(map(int, input().split())) for _ in range(n)]
+
+# 익은 토마토의 위치를 찾아 저장한다.
+q = deque()
+for i in range(n):
+    for j in range(m):
+        if box[i][j] == 1:
+            q.append((i, j))
+bfs()
+
+ans = 0
+for tomato_list in box:
+    for tomato in tomato_list:
+        if tomato == 0:
+            print(-1)
+            exit()
+    # print(f"max({ans}, max({tomato_list}))")
+    ans = max(ans, max(tomato_list))
+
+print(ans - 1)
+# 10
+
+# 1 -1 7  6  5 4
+# 0 -1 6  5  4 3
+# 9  8 7  6 -1 2
+# 0  0 8  7 -1 1
+
+# 1 -1 0 0 5 4
+# 2 -1 0 5 4 3
+# 3  4 5 0 -1 2
+# 4  5 0 0 -1 1
--- 알고리즘/test.py
+++ 알고리즘/test.py
@@ -1,31 +1,31 @@
-# from pprint import pprint
-import sys
+from datetime import datetime
 
-sys.setrecursionlimit(10**6)  # 재귀함수 깊이설정! 이거안하면 안풀림
-input = sys.stdin.readline
-
-
-def dfs(x, y):
-    visited[x][y] = 1  # 방문표시
-    for dx, dy in [(1, 0), (-1, 0), (0, 1), (0, -1)]:  # 4방향탐색
-        nx = x + dx
-        ny = y + dy
-        if arr[nx][ny] == 1 and not visited[nx][ny]:
-            dfs(nx, ny)
+# print(datetime.now())
+# Now = datetime.now()
+# print(Now.minute == 7)
+# go=0
+# if Now.weekday() == 2 and Now.hour==23 and Now.minute == 55:
+#     go=1
+# else:go=0
 
 
-T = int(input())
-for _ in range(T):  # 테스트 케이스만큼 반복
-    m, n, k = map(int, input().split())  # 가로, 세로, 좌표개수
-    arr = [[0] * (m + 1) for _ in range(n + 1)]
-    visited = [[0] * (m + 1) for _ in range(n + 1)]
-    for _ in range(k):  # 좌표 입력받아 1표시
-        x, y = map(int, input().split())
-        arr[y][x] = 1
-    cnt = 0
-    for i in range(n):
-        for j in range(m):
-            if arr[i][j] and not visited[i][j]:  # 배열에 값이 1이고, 미방문
-                dfs(i, j)
-                cnt += 1
-    print(cnt)
+# @tasks.loop(hours=1)
+# async def send_sunday_notification():
+#     # 현재 시간과 요일 확인
+#     Now = datetime.now()
+#     # weekday = Now.weekday()
+#     # hour = Now.hour
+#     if Now.weekday() == 3 and Now.minute == 7:
+#         print(11)
+Now = datetime.now()
+sunday = Now.weekday()
+print(Now.hour == 0, sunday == 3)
+
+# @tasks.loop(minute=1)
+#   async def printer(self):
+#     Now = datetime.now()
+#     sunday = Now.weekday()
+#     T = Now.hour
+#     if sunday == 10 and T ==0:
+#       channel = client.get_channel(1091978809110171682)
+#       await channel.send("일요일 오후 4시입니다! 이번 주 일정을 확인하세요.")
