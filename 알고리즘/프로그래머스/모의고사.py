7d66d2417f50e51e5dcaf6c2f80783dd763e7606
taehopark <qkrxogh7@gmail.com>
2023-04-11 18:09:25 +0900

    그리디..알고

--- 알고리즘/프로그래머스/모의고사.py
+++ 알고리즘/프로그래머스/모의고사.py
@@ -1,29 +1,18 @@
 def solution(answers):
-    p1 = [1, 2, 3, 4, 5]
-    p2 = [2, 1, 2, 3, 2, 4, 2, 5]
-    p3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
-    p_list = [p1, p2, p3]
-    dic = {"1": 0, "2": 0, "3": 0}
-    ans = []
-    for idx in range(len(answers)):
-        for p_dix in range(len(p_list)):
-            if answers[idx] == p_list[p_dix][idx]:
-                dic_idx = str(1 + p_dix)
-                dic[dic_idx] += 1
-    values = list(dic.values())
-    print(values)
-    if 1 < values.count(max(values)):
-        # print('값이 여러개')
-        for _ in range(values.count(max(values))):
-            ans.append(1 + values.index(max(values)))
-            values.index(max(values))
+    pattern1 = [1, 2, 3, 4, 5]
+    pattern2 = [2, 1, 2, 3, 2, 4, 2, 5]
+    pattern3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
+    score = [0, 0, 0]
+    result = []
 
-    else:
-        ans.append(1 + values.index(max(values)))
-    # print(dic)
-
-    return ans
-
-
-solution([1, 2, 3, 4, 5])
-solution([1, 3, 2, 4, 2])
+    for idx, answer in enumerate(answers):
+        if answer == pattern1[idx % len(pattern1)]:
+            score[0] += 1
+        if answer == pattern2[idx % len(pattern2)]:
+            score[1] += 1
+        if answer == pattern3[idx % len(pattern3)]:
+            score[2] += 1
+    for idx, s in enumerate(score):
+        if s == max(score):
+            result.append(idx + 1)
+    return result
