19a61cfd646febfb9dae23925175eb267ea2ab81
taehopark <qkrxogh7@gmail.com>
2023-05-13 14:55:40 +0900

    algo

--- 알고리즘/test.py
+++ 알고리즘/test.py
@@ -1,6 +1,20 @@
-for case in range(1, int(input()) + 1):
-    st = input()
-    for s in st:
-        if s in "aeiou":
-            st = st.replace(s, "")
-    print(f"#{case} {st}")
+def dfs(n, sm):
+    global ans
+    if sm > K:
+        return
+    if n == N:
+        if K == sm:
+            ans += 1
+        return
+    # 선택
+    dfs(n + 1, sm + nums[n])
+    # 선택안함
+    dfs(n + 1, sm)
+
+
+for case in range(1, 1 + int(input())):
+    N, K = map(int, input().split())
+    nums = list(map(int, input().split()))
+    ans = 0
+    dfs(0, 0)
+    print(ans)
