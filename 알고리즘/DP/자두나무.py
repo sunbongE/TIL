ad9d84fdbcdecb08cad71a5827830252d42c3d91
taehopark <qkrxogh7@gmail.com>
2023-05-27 18:04:36 +0900

    백트래킹.

--- 알고리즘/DP/자두나무.py
+++ 알고리즘/DP/자두나무.py
@@ -1,34 +1,45 @@
-T, W = map(int, input().split())
+import sys
 
+# 자두가 떨어지는 시간 T와 이동할 수 있는 횟수 W
+t, w = map(int, sys.stdin.readline().rstrip().split(" "))
 
-plums = [0]
-dp = [[0 for _ in range(W + 1)] for _ in range(T + 1)]
+# 자두가 떨어지는 나무의 번호 저장
+arr = [0]
+for _ in range(t):
+    arr.append(int(sys.stdin.readline()))
 
-for _ in range(T):
-    plums.append(int(input()))
+# 2차원 DP 테이블 초기화
+dp = [[0] * (w + 1) for _ in range(t + 1)]
 
-for i in range(1, T + 1):
-    # 한번도 움직이지 않았을 때(dp[i][0]을 채우는 과정)
-    if plums[i] == 1:  # 자두가 1번에서 떨어질 때만 받아 먹을 수 있다.
+for i in range(1, t + 1):
+    # 1번 나무에서 한 번도 움직이지 않는 경우
+
+    # 1번 나무에서 자두가 떨어진다면
+    if arr[i] == 1:
         dp[i][0] = dp[i - 1][0] + 1
+
+    # 2번 나무에서 자두가 떨어진다면
     else:
         dp[i][0] = dp[i - 1][0]
 
-    # 이동 횟수를 1번부터 W 번까지 움직이면서 체크
-    for j in range(1, W + 1):
+    # 1번 이상 움직이는 경우에 대해 탐색
+    for j in range(1, w + 1):
+        # i초에 2번 나무에서 자두가 떨어지고, 움직인 위치가 1번 나무면 즉, 현 위치
+        if arr[i] == 2 and j % 2 == 1:
+            # 이전 위치로부터 움직여서 받아 먹을 것인지, 현재 위치에서 받아 먹을 것인지를 비교
+            dp[i][j] = max(dp[i - 1][j - 1], dp[i - 1][j]) + 1
 
-        # 자두가 1번에서 떨어지고, 그것을 받아 먹기
-        if plums[i] == 1 and j % 2 == 0:
-            # 움직여서 받아먹을 것인가, 현재위치에서 받아먹을 것인가
-            # 어짜피 이동한 횟수는 같다(지금 이동하거나 이전에 이동했거나)
-            dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - 1]) + 1
+        # i초에 1번 나무에서 자두가 떨어지고, 현재 1번 나무에 위치해있다면
+        elif arr[i] == 1 and j % 2 == 0:
+            # 이전 위치로부터 움직여서 받아 먹을 것인지, 현재 위치에서 받아 먹을 것인지를 비교
+            dp[i][j] = max(dp[i - 1][j - 1], dp[i - 1][j]) + 1
 
-        # 자두가 2번에서 떨어지고, 그것을 받아 먹기
-        elif plums[i] == 2 and j % 2 == 1:
-            dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - 1]) + 1
-
-        # 그 외 - 안먹는다 - 그대로 있거나 움직여서 안먹음
+        # i초에 자두가 떨어지는 나무와 현재 나무의 위치가 다르다면
         else:
-            dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - 1])
+            # 움직여서 못 먹는 경우와 움직이지 않아서 못 먹는 경우를 비교
+            dp[i][j] = max(dp[i - 1][j - 1], dp[i - 1][j])
+        # print(arr[i])
+        # print(dp)
 
-print(max(dp[-1]))
+# DP 테이블의 마지막 행의 최댓값을 출력
+print(max(dp[t]))
