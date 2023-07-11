ecc6380642bc349af85cd393962f5a38b93f461a
taehopark <qkrxogh7@gmail.com>
2023-07-11 23:18:08 +0900

    java문법학습

--- 알고리즘/test1.py
+++ 알고리즘/test1.py
@@ -1,13 +1,13 @@
-# 최빈수구하기.
-for _ in range(1, 1 + int(input())):
-    case = int(input())
-    nums = list(map(int, input().split()))
-    nums.sort(reverse=True)
-    dogam = dict()
-    for num in nums:
-        dogam[num] = dogam.get(num, 0) + 1
-    target = max(dogam.values())
-    for k, v in dogam.items():
-        if v == target:
-            print(f"#{case} {k}")
-            break
+# # 최빈수구하기.
+# for _ in range(1, 1 + int(input())):
+#     case = int(input())
+#     nums = list(map(int, input().split()))
+#     nums.sort(reverse=True)
+#     dogam = dict()
+#     for num in nums:
+#         dogam[num] = dogam.get(num, 0) + 1
+#     target = max(dogam.values())
+#     for k, v in dogam.items():
+#         if v == target:
+#             print(f"#{case} {k}")
+#             break
